2f3591795ab71819c53fed08c80011a8c1b7fe31
Michael Lai <mrlai7782@gmail.com>
2021-06-15 10:00:15 -0700

    update

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,8 +1,19 @@
 const express = require('express');
+const db = require('../db/db');
 const router = express.Router();
+const axios = require('axios');
 
-router.get('/', (req, res) => {
-  res.json('this is api!');
+router.get('/', async (req, res) => {
+  try {
+    const response = await axios({
+      method: 'get',
+      url: `http://omdbapi.com/?apikey=${process.env.apiKey}&t=inception`,
+    });
+    console.log(response.data);
+    res.json(response.data);
+  } catch (error) {
+    console.log(error.message);
+  }
 });
 
 module.exports = router;
