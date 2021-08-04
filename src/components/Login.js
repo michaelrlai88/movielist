6786a551bd0ed7d2654ddeee9d4bf10ec20ef3e1
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-04 01:28:51 -0700

    fix cors

--- src/components/Login.js
+++ src/components/Login.js
@@ -110,6 +110,8 @@ const Login = () => {
         },
       });
 
+      console.log(response.data);
+
       if (response.data.token) {
         localStorage.setItem('token', response.data.token);
         dispatch(authTrue());
