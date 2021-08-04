1d6781b56625087a79b34ccdf81d7156f29644b0
michaelrlai88 <michaelrlai88@gmail.com>
2021-08-03 23:43:12 -0700

    fixed api url

--- src/components/Search.js
+++ src/components/Search.js
@@ -140,6 +140,8 @@ const Search = () => {
     process.env.REACT_APP_LOCAL_URL || 'https://movielist88.herokuapp.com';
 
   useEffect(() => {
+    console.log(url);
+
     const searchTitle = async () => {
       try {
         const response = await axios({
